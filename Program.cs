6ef2e635544238894c99934f48ddcbee00101cad
DAAN <daanmgneni@gmail.com>
2023-04-01 09:03:33 +0530

    Greeting Msg

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace BinarySearchTreeProgram
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("*****Welcome to the Hash Table and Bst problem*****");
+        }
+    }
+}
